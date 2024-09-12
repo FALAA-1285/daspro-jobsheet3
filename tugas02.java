6de1b7b2a913b64fa0f92d1e161f9901bf8605d7
FALAA-1285 <ahmadfalahi1285@gmail.com>
2024-09-12 12:04:15 +0700

    mengerjakkan tugak no-2

--- tugas02.java
+++ tugas02.java
@@ -1,3 +1,23 @@
+
+import java.util.Scanner;
+
 public class tugas02 {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int jamKerja, upahPerJam;
+        double bonus, pajak, jumlahGaji, gaji;
+
+        System.out.print("Masukkan Jumlah jam kerja karyawan: ");
+        jamKerja= input.nextInt();
+        System.out.print("Masukkan Upah karyawan per Jam: ");
+        upahPerJam= input.nextInt();
+
+        double kerja =upahPerJam*jamKerja;
+        bonus=kerja*0.1;
+        pajak=kerja*0.05;
+        double gajibersih=kerja+bonus-pajak;
+
+        System.out.println("Jumlah gaji bersih karyawan: "+gajibersih);
+    }
 }
